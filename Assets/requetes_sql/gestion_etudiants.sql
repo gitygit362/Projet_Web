33a560c6c56d325c1ad2930324f31fb5a5500804
Redyfly <hirlibaptiste@gmail.com>
2024-03-14 09:27:33 +0100

    SFx 17 et 18 (à finir)

--- Assets/requetes_sql/gestion_etudiants.sql
+++ Assets/requetes_sql/gestion_etudiants.sql
@@ -1,8 +1,35 @@
 /* SFx 17 - Rechercher un compte Etudiant */
 
+SELECT ID_utilisateur, nom, prenom, nom_centre, promo
+FROM Utilisateurs
+JOIN Etudiant on Etudiant.ID_utilisateur = Utilisateurs.ID_utilisateur
+JOIN Centre on Centre.ID_centre = Etudiant.ID_centre
+JOIN contient on contient.ID_centre = Centre.ID_centre
+JOIN Promo on Promo.ID_promo = contient.ID_promo
+WHERE nom = "Textbox_nom"
+    AND prenom = "Textbox_prenom"
+    AND nom_centre = "Textbox_centre"
+    AND promo = "Textbox_promo"
+;
 
 /* SFx 18 - Créer un compte Etudiant */
 
+BEGIN TRANSACTION;
+
+INSERT INTO Utilisateurs
+VALUES ('Test1_nom', 'Test1_prenom', 'Test1_password')
+;
+
+INSERT INTO Etudiant DEFAULT VALUES;
+
+DECLARE @userID INT;
+SET @userID = SCOPE_IDENTITY();
+
+INSERT INTO etudiant (id_utilisateur)
+VALUES (@id_utilisateur)
+;
+
+COMMIT;
 
 /* SFx 19 - Modifier un compte Etudiant */
 
