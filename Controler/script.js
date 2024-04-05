dbc16ab76eb25a3d808ae33552d3fc549f99ba61
FlorianLantz <florian.lantz@viacesi.fr>
2024-04-05 11:35:03 +0200

    Mentions legales

--- Controler/script.js
+++ Controler/script.js
@@ -1,9 +1,10 @@
+/*
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('/Projet_Web/ServiceWorker.js')
     .then( (sw) => console.log('Le Service Worker a été enregistrer', sw))
     .catch((err) => console.log('Le Service Worker est introuvable !!!', err));
     }
-
+*/
 
 
 
